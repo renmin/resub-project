93874ce264267da71ef6c9c8e3d037fc925ecb1b
Min Ren <minren@microsoft.com>
2023-12-26 16:01:08 +0800

    do not use strictMode

--- src/index.tsx
+++ src/index.tsx
@@ -9,11 +9,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <React.Fragment>
         <App />
 
     <Counter />
-  </React.StrictMode>
+  </React.Fragment>
 );
 
 // If you want to start measuring performance in your app, pass a function
